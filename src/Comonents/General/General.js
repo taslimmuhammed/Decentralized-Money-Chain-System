0e7145e7870f817d92f3dac2d7a1165448d4136d
taslimmuhammed <taslimmuhammed67@gmail.com>
2022-06-06 19:07:14 +0530

    genral loading fix

--- src/Comonents/General/General.js
+++ src/Comonents/General/General.js
@@ -2,6 +2,7 @@ import React,{useState, useEffect, useContext} from 'react'
 import './General.css'
 import {EthersContext} from '../../Context/EthersContext'
 import { useNavigate } from 'react-router-dom'
+import Loader from '../Loading/Loading'
 function General() {
   const navigate = useNavigate()
   const {checkSignIn,unitBalance,buyToken,enterGame,unitCount, currentAccount} = useContext(EthersContext)
@@ -45,7 +46,6 @@ const initiaor= async()=>{
     setUnits(units)
     const bunits = await unitCount()
     let balance = bunits- units/100
-
     setBUnits(balance)
     setBunit1(bunits)
   }catch(e){
@@ -58,32 +58,33 @@ useEffect(() => {
   initiaor()
 }, [])
   return (
-    <div className='p_main'>
-    <div className='p_head'>General Member</div>
-    <div className='Wallet'>
-        <div className='wallet_head'>WALLET ADDRESS</div>
-        <div className='wallet_address'>{currentAccount}</div>
-    </div>
+  isLoading?<Loader/>:<div className='p_main'>
+  <div className='p_head'>General Member</div>
+  <div className='Wallet'>
+      <div className='wallet_head'>WALLET ADDRESS</div>
+      <div className='wallet_address'>{currentAccount}</div>
+  </div>
 
-    <div className='p_bottom'>
+  <div className='p_bottom'>
 
-        <div className='sub_head'>Buy Unit</div>
-        <div className='sub_sub'>{Units/100}/1</div>
-      
-        <div className='sub_head'>Out Unit</div>
-        <div className='sub_sub'>{BUnits}/{Bunit1}</div>
+      <div className='sub_head'>Buy Unit</div>
+      <div className='sub_sub'>{Units/100}/1</div>
+    
+      <div className='sub_head'>Out Unit</div>
+      <div className='sub_sub'>{BUnits}/{Bunit1}</div>
 
-        <div className='sub_head'>Price of 1 Unit</div>
-            <div className='sub_sub'>10 USDt (*polygon chain)</div>
-        <div className='p_buttons' >
-            <button className="button-9" role="button" onClick={handleBuy}>Buy UNIT</button>
-            <button className="button-9" role="button" onClick={handleLot}>LOT</button>
-            </div>
-       
-    </div>
+      <div className='sub_head'>Price of 1 Unit</div>
+          <div className='sub_sub'>10 USDt (*polygon chain)</div>
+      <div className='p_buttons' >
+          <button className="button-9" role="button" onClick={handleBuy}>Buy UNIT</button>
+          <button className="button-9" role="button" onClick={handleLot}>LOT</button>
+          </div>
+     
+  </div>
 
 </div>
   )
+    
 }
 
 export default General
